43f948e2e4821977344f2ae181cff777c45db9ca
Usman Tahir <Usman.tahir@synergy-it.com>
2025-02-16 11:34:13 +0500

    17. Implementing gRPC Service Class which Inherits from gRPC generated service

---
+++ GrpcHelloWorldServer/Services/HelloWorldService.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using GrpcHelloWorldServer.Protos;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GrpcHelloWorldServer.Services
+{
+    public class HelloWorldService :HelloService.HelloServiceBase
+    {
+        private readonly ILogger<HelloWorldService> _logger;
+
+        public HelloWorldService(ILogger<HelloWorldService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HellosResponse> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            string resultMessage = $"Hello {request.Name}";
+
+            var response =new HellosResponse()
+            {
+                Message = resultMessage,
+            };
+            return Task.FromResult(response);
+        }
+    }
+}
