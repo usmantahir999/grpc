7f928aa392032910faa23f9d3cf137d99ebca68d
Usman Tahir <Usman.tahir@synergy-it.com>
2025-02-19 21:59:35 +0500

    21. Consume Grpc HelloService API From Client Console Application with GrpcChannel

--- GrpcHelloWorldClient/Program.cs
+++ GrpcHelloWorldClient/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using GrpcHelloWorldClient.Protos;
+using System;
+using System.Threading.Tasks;
 
 namespace GrpcHelloWorldClient
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new HelloService.HelloServiceClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Hello Usman Client" });
+            Console.WriteLine($"Greeting: {reply.Message}");
+            Console.ReadKey();
         }
     }
 }
